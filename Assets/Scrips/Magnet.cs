56a2f1d26f1246bc86777b124dc1a98b74449665
juan <juampats12345@gmail.com>
2024-11-15 00:19:11 +0100

    imanes se atraen unos a otros

--- Assets/Scrips/Magnet.cs
+++ Assets/Scrips/Magnet.cs
@@ -6,43 +6,66 @@ using UnityEngine;
 
 public class Magnet : MonoBehaviour
 {
+    public GameObject parent;
     public Horientation horientation;
     public float magnetStrenht;
     public LayerMask magnetLayer;
     public Rigidbody2D rb;
-    public List<GameObject> magnetsAround;
+    public List<Transform> magnetsAround;
     public List<Horientation> magnetsAroundH;
 
 
     public void OnTriggerEnter2D(Collider2D col)
     {
+        if(col.gameObject == parent )
+        {
+            return;
+        }
+
         if ((magnetLayer.value & (1 << col.gameObject.layer)) != 0)
         {
-            magnetsAround.Add(col.gameObject);
+            magnetsAround.Add(col.gameObject.transform);
             magnetsAroundH.Add(col.gameObject.GetComponent<Horientation>());
         }
     }
 
     public void OnTriggerExit2D(Collider2D col)
     {
-        if (magnetsAround.Contains(col.gameObject))
+        if (magnetsAround.Contains(col.gameObject.transform))
         {
             magnetsAroundH.Remove(col.gameObject.GetComponent<Horientation>());
-            magnetsAround.Remove(col.gameObject);
+            magnetsAround.Remove(col.gameObject.transform);
         }
     }
 
     public void Update()
     {
-        for( int magnetIndex = 0; magnetIndex < magnetsAround.Count; magnetIndex++ )
+        for (int magnetIndex = 0; magnetIndex < magnetsAround.Count; magnetIndex++)
         {
-            treatMaget( magnetsAround[magnetIndex], magnetsAroundH[magnetIndex] );
+            treatMaget(magnetsAround[magnetIndex], magnetsAroundH[magnetIndex]);
         }
     }
 
-    public void treatMaget( GameObject targetMagnet, Horientation targetHor )
+    public void treatMaget(Transform targetMagnet, Horientation targetHor)
     {
+
+        float distance = Vector3.Distance(transform.position, targetMagnet.position);
+        float forceMultiplier = magnetStrenht;
         bool isAtractive = horientation.isNorth == targetHor.isNorth;
+
+        if (!isAtractive)
+        {
+            forceMultiplier = -forceMultiplier;
+        }
+
+        float force = forceMultiplier * magnetStrenht / (distance * distance);
+
+        if (targetMagnet.position.y < transform.position.y)
+        {
+            force = -force;
+        }
+
+        rb.AddForce(Vector2.up * force * Time.deltaTime);
     }
 
 }
