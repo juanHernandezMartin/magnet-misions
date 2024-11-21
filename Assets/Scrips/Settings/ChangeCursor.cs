588e77e739941434a5c14fa2f3124cbf0b6cd2f7
juan <juampats12345@gmail.com>
2024-11-21 19:41:57 +0100

    cursorCustom y creacion escenas

---
+++ Assets/Scrips/Settings/ChangeCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    public GameObject cursorP1Prefab;
+    private GameObject customCursorP1;
+
+    public void Start()
+    {
+        Cursor.visible = false;
+        customCursorP1 = Instantiate( cursorP1Prefab, transform.position, transform.rotation);
+    }
+
+    public void Update()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = -2;
+        customCursorP1.transform.position = worldPosition;
+    }
+}
