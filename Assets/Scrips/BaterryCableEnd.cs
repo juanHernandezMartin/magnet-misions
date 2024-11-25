d9020826aa52044de32167d7444248b547efaf93
juan <juampats12345@gmail.com>
2024-11-25 02:58:09 +0100

    cosas de la bateria

--- Assets/Scrips/BaterryCableEnd.cs
+++ Assets/Scrips/BaterryCableEnd.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class BaterryCableEnd : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //[HideInInspector]
+    public bool isPlugedLeftCable;
+    public bool isPlugedRightCable;
+    public GameObject currCable;
+
+    public void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.CompareTag("LeftCable"))
+        {
+            currCable = col.gameObject;
+        }
+
+        if (col.CompareTag("RightCable"))
+        {
+            currCable = col.gameObject;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnMouseUp()
     {
-        
+        if (currCable != null)
+        {
+            if (currCable.CompareTag("LeftCable"))
+            {
+                isPlugedLeftCable = true;
+            }
+
+            if (currCable.CompareTag("RightCable"))
+            {
+                isPlugedRightCable = true;
+            }
+
+            currCable.transform.position = transform.position;
+        }
     }
+
+    public void OnMouseDown()
+    {
+        isPlugedLeftCable = false;
+        isPlugedRightCable = false;
+    }
+
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("LeftCable"))
+        {
+            currCable = null;
+        }
+
+        if (col.CompareTag("RightCable"))
+        {
+            currCable = null;
+        }
+    }
+
 }
---
+++ Assets/Scrips/BaterryScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaterryScript : MonoBehaviour
+{
+    public BaterryCableEnd leftCable;
+    public BaterryCableEnd rightCable;
+    public Magnet mainMagnet;
+
+    public void Update()
+    {
+        
+    }
+}
