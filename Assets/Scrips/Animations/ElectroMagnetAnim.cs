68da3582b4e640525eae4aa03be11dc243844966
juan <juampats12345@gmail.com>
2024-12-27 16:58:58 +0100

    electromagnet animation

--- Assets/Scrips/Animations/ElectroMagnetAnim.cs
+++ Assets/Scrips/Animations/ElectroMagnetAnim.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [Serializable]
 public struct AnimTimes
 {
-    public float batery;
-    public float electroMagnet;
+    public float bateryTime;
+    public float electroMagnetTime;
 }
 
 
@@ -15,11 +16,17 @@ public struct AnimTimes
 public class ElectroMagnetAnim : MagnetAnim
 {
     public GameObject electroMagnetLeft;
-    public GameObject magnetRight;
-    public GameObject battery;
-    public GameObject batteryAnchor;
+    public Transform magnetRight;
+    public Transform battery;
+    public Transform batteryPlug;
     public bool animAtStart;
     public AnimTimes animTimes;
+    public GameObject ActivatedMode;
+
+    private Vector3 originalBatteryPosition;
+    private Vector3 originalPlugPosition;
+    private Vector3 electroMagnetOriginalPosition;
+    private Vector3 rightMagnetOriginalPosition;
 
     private float scaleMult = 4.8f;
 
@@ -29,21 +36,92 @@ public class ElectroMagnetAnim : MagnetAnim
         {
             AnimateMagnets();
         }
+
+        originalPlugPosition = batteryPlug.position;
+        originalBatteryPosition = battery.position;
+        electroMagnetOriginalPosition = electroMagnetLeft.transform.position;
+        rightMagnetOriginalPosition = magnetRight.position;
     }
 
     public new void AnimateMagnets()
     {
-        /*
-        float targetY = (magnetTop.transform.position.y + magnetBot.transform.position.y) /2f;
-
-        float targetYTop = targetY + scaleMult*magnetBot.transform.localScale.x;
-        Tween topTween = magnetTop.transform.DOMoveY( targetYTop, animTime );
-        topTween.SetEase(Ease.InExpo);
-
-        float targetYBot = targetY - scaleMult*magnetBot.transform.localScale.x;
-        Tween botTween = magnetBot.transform.DOMoveY( targetYBot, animTime );
-        botTween.SetEase(Ease.InExpo);
-        */
+        ActivatedMode.SetActive(false);
+        //Tween batteryTween = battery.DOMove(batteryPlug.position, animTimes.bateryTime);
+        Tween batteryTween = batteryPlug.DOMove(battery.position, animTimes.bateryTime);
+        batteryTween.OnComplete(() =>
+        {
+            AtractElectroMagnet();
+        });
     }
+
+    public void AtractElectroMagnet()
+    {
+        Vector3 eulersOfMagnetInside = ActivatedMode.transform.eulerAngles;
+        eulersOfMagnetInside.z += 180;
+        ActivatedMode.transform.eulerAngles = eulersOfMagnetInside;
+
+        ActivatedMode.SetActive(true);
+        float targetX = (electroMagnetLeft.transform.position.x + magnetRight.transform.position.x) / 2f;
+
+        float targetXLeft = targetX - scaleMult * electroMagnetLeft.transform.localScale.x;
+        Tween leftTween = electroMagnetLeft.transform.DOMoveX(targetXLeft, animTimes.electroMagnetTime);
+        leftTween.SetEase(Ease.InExpo);
+
+        float targetXRight = targetX + scaleMult * electroMagnetLeft.transform.localScale.x;
+        Tween rightTween = magnetRight.transform.DOMoveX(targetXRight, animTimes.electroMagnetTime);
+        rightTween.SetEase(Ease.InExpo);
+
+        rightTween.OnComplete(() =>
+        {
+            ActivatedMode.SetActive(false);
+            AnimateBaterryBack(false);
+        });
+    }
+
+    public void RepelElectroMagnet()
+    {
+        Vector3 eulersOfMagnetInside = ActivatedMode.transform.eulerAngles;
+        eulersOfMagnetInside.z += 180;
+        ActivatedMode.transform.eulerAngles = eulersOfMagnetInside;
+        ActivatedMode.SetActive(true);
+        Tween leftTween = electroMagnetLeft.transform.DOMoveX(electroMagnetOriginalPosition.x, animTimes.electroMagnetTime);
+        //leftTween.SetEase(Ease.InExpo);
+
+        Tween rightTween = magnetRight.transform.DOMoveX(rightMagnetOriginalPosition.x, animTimes.electroMagnetTime);
+        //rightTween.SetEase(Ease.InExpo);
+
+        rightTween.OnComplete(() =>
+        {
+            ActivatedMode.SetActive(false);
+            AnimateBaterryBack(true);
+        });
+    }
+
+    public void AnimateBaterryBack(bool isNextAtract)
+    {
+        Tween moveBackBaterry = batteryPlug.DOMove(originalPlugPosition, animTimes.bateryTime);
+        moveBackBaterry.OnComplete(() =>
+        {
+            Vector3 baterryRotation = battery.eulerAngles;
+            baterryRotation.z += 180;
+            Tween rotateBaterry = battery.DORotate(baterryRotation, animTimes.bateryTime);
+            rotateBaterry.OnComplete(() =>
+            {
+                Tween maoveInBaterry = batteryPlug.DOMove(battery.position, animTimes.bateryTime);
+                maoveInBaterry.OnComplete(() =>
+                {
+                    if (isNextAtract)
+                    {
+                        AtractElectroMagnet();
+                    }
+                    else
+                    {
+                        RepelElectroMagnet();
+                    }
+                });
+            });
+        });
+    }
+
 }
 
